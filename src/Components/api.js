b93ffaef4fd94a4f4ccb64fd28ff4c1a35de4c7f
Muhammad-Huzaifa-786 <2008huzaifakhi@gmail.com>
2025-01-28 03:40:03 +0500

    1

--- src/Components/api.js
+++ src/Components/api.js
@@ -2,16 +2,9 @@ import axios from 'axios';
 
 // Base configuration for Axios
 const API = axios.create({
-    baseURL: 'https://backendhackathon-production.up.railway.app/', // Backend API base URL
+    baseURL: 'https://backendhackathon-production.up.railway.app', // Backend API base URL
 });
 
-// Add an Authorization header for protected routes if token exists
-API.interceptors.request.use((req) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        req.headers.Authorization = `Bearer ${token}`;
-    }
-    return req;
-});
+
 
 export default API;
