5a26daa0b59e2ec435908d1452d170d12671ec7a
Muhammad-Huzaifa-786 <2008huzaifakhi@gmail.com>
2025-01-28 14:32:21 +0500

    1

--- src/Components/api.js
+++ src/Components/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Base configuration for Axios
 const API = axios.create({
     // baseURL: 'http://localhost:5000/api', // Backend API base URL
-    baseURL: 'https://backendhackathon-production.up.railway.app/api', // Backend API base URL
+    baseURL: 'https://backend-production-64be.up.railway.app/api', // Backend API base URL
 });
 
 
--- src/Functions/Url.js
+++ src/Functions/Url.js
@@ -1,2 +1,2 @@
 // export const BaseUrl = 'http://localhost:5000/api'
-export const BaseUrl = 'https://backendhackathon-production.up.railway.app/api'
+export const BaseUrl = 'https://backend-production-64be.up.railway.app/api'
